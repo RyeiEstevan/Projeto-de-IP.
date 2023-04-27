162fc81370871d93b274cbebc09e764a84828248
Flávio Roberto <124847034+MusicalUsagi@users.noreply.github.com>
2023-04-27 18:09:32 -0300

    Add files via upload

--- mapa.py
+++ mapa.py
@@ -1,4 +1,5 @@
 import pygame
+from constantes import *
 
 class Mapa:
     def __init__(self, altura, largura, largura_menu, largura_bordas):
@@ -8,20 +9,29 @@ class Mapa:
         self.largura = largura
         self.largura_menu = largura_menu
         self.largura_bordas = largura_bordas
-        
+        self.background = pygame.image.load("wallpaperflare.com_wallpaper.jpg")
 
         #Criando a tela do jogo e dando nome a ela
         self.tela = pygame.display.set_mode((largura, altura)) 
-        self.tela.fill((0, 153, 0)) 
+        self.tela.blit(self.background, (0, 0))
         pygame.display.flip()
         pygame.display.set_caption("CINberman (nome temporario)")
         self.bordas = []
-        #Criando as 4 bordas e salvando elas numa lista para checar colisão depois
+        
     def desenhar_bordas(self):
         cor_borda = (80, 80, 80)
-        self.bordas = [pygame.draw.line(self.tela, cor_borda, (0, self.largura_menu+int(self.largura_bordas/2)), (self.largura, self.largura_menu+int(self.largura_bordas/2)), self.largura_bordas),
-        pygame.draw.line(self.tela, cor_borda, (int(self.largura_bordas/2), self.largura_menu+int(self.largura_bordas/2)), (int(self.largura_bordas/2), self.altura), self.largura_bordas),
-        pygame.draw.line(self.tela, cor_borda, (0, self.altura-int(self.largura_bordas/2)), (self.largura, self.altura-int(self.largura_bordas/2)), self.largura_bordas), 
-        pygame.draw.line(self.tela, cor_borda, (self.largura-int(self.largura_bordas/2), self.largura_menu+int(self.largura_bordas/2)), (self.largura-int(self.largura_bordas/2), self.altura), self.largura_bordas)]
+
+        #Criando as 4 bordas e salvando elas numa lista para checar colisão depois
+        for i in range (1, ((TAMANHO_CELULA*CELULAS_ALTURA)-TAMANHO_MENU)//TAMANHO_BORDAS):
+            self.bordas.append(pygame.draw.rect(self.tela, cor_borda, (0, i*TAMANHO_BORDAS+TAMANHO_MENU, TAMANHO_BORDAS, TAMANHO_BORDAS)))
+            self.bordas.append(pygame.draw.rect(self.tela, cor_borda, ((CELULAS_LARGURA*TAMANHO_CELULA)-TAMANHO_BORDAS, i*TAMANHO_BORDAS+TAMANHO_MENU, TAMANHO_BORDAS, TAMANHO_BORDAS)))
         
+        for a in range (0, (TAMANHO_CELULA*CELULAS_LARGURA)//TAMANHO_BORDAS):
+            self.bordas.append(pygame.draw.rect(self.tela, cor_borda, (a*TAMANHO_BORDAS, TAMANHO_MENU, TAMANHO_BORDAS, TAMANHO_BORDAS)))
+            self.bordas.append(pygame.draw.rect(self.tela, cor_borda, (a*TAMANHO_BORDAS, (CELULAS_ALTURA*TAMANHO_CELULA)-TAMANHO_BORDAS, TAMANHO_BORDAS, TAMANHO_BORDAS)))
+
+
+
+
+
         
