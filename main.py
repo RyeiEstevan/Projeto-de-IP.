1817a4e5e0ee2e77cb1089acaf4a125b0f27af95
Flávio Roberto <124847034+MusicalUsagi@users.noreply.github.com>
2023-04-30 16:27:12 -0300

    Atualização: Sprites
    Modificação das classes do mapa, blocos e personagem para agora terem sprites.

--- main.py
+++ main.py
@@ -1,38 +1,35 @@
 import pygame
 from constantes import *
 from mapa import Mapa
+from mapa import Bordas
 from blocos import Blocos_indestrutiveis
-from personagem import personagem
+from personagem import Personagem
 
 
 def main():
 
-    #Criando mapa e blocos que não quebram
+    #Criando objetos e o grupo de seus sprites
+    sprites = pygame.sprite.Group()
     mapa = Mapa(CELULAS_ALTURA*TAMANHO_CELULA, CELULAS_LARGURA*TAMANHO_CELULA, TAMANHO_MENU, TAMANHO_BORDAS) 
-    blocos = Blocos_indestrutiveis(mapa.tela)
-    player = personagem(mapa.tela, 25, 125 )
+    bordas = [Bordas(i[0], i[1]) for i in mapa.bordas]
+    blocos_ind = [Blocos_indestrutiveis(i[0], i[1]) for i in blocos_indestrutiveis]
+    player = Personagem(mapa.tela, 25, 125)
+    [sprites.add(i) for i in bordas+blocos_ind+[player]]
+
     #Iniciando pygame e loop para o jogo ficar rodando até fecharem
+    clock = pygame.time.Clock()
     Rodar = True
     pygame.init()
     while Rodar:
-        mapa.tela.fill((0, 153, 0)) 
-        blocos.desenhar_blocos()
-        mapa.desenhar_bordas()
+        mapa.tela.blit(mapa.background, (0, TAMANHO_BORDAS+TAMANHO_MENU))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 Rodar == False
                 exit()
 
-            if event.type == pygame.KEYDOWN:
-                tecla = pygame.key.get_pressed()
-                player.movimentacao(tecla)
-            
-            
 
-
-        
-        blocos.desenhar_blocos()
-        mapa.desenhar_bordas()
-        player.desenhar_personagem()
+        sprites.draw(mapa.tela)
+        sprites.update()
         pygame.display.update()
+        clock.tick(60)
 main()
--- mapa.py
+++ mapa.py
@@ -3,32 +3,40 @@ from constantes import *
 
 class Mapa:
     def __init__(self, altura, largura, largura_menu, largura_bordas):
-        
+
         #Definindo propriedades do mapa
         self.altura = altura
         self.largura = largura
         self.largura_menu = largura_menu
         self.largura_bordas = largura_bordas
-        
+        self.background = pygame.image.load("sprites\Back.jpg")
 
         #Criando a tela do jogo e dando nome a ela
         self.tela = pygame.display.set_mode((largura, altura)) 
-        self.tela.fill((0, 153, 0)) 
-        pygame.display.flip()
+        #pygame.display.flip()
         pygame.display.set_caption("CINberman (nome temporario)")
         self.bordas = []
-
+       
         #Criando as 4 bordas e salvando elas numa lista para checar colisão depois
         for i in range (1, ((TAMANHO_CELULA*CELULAS_ALTURA)-TAMANHO_MENU)//TAMANHO_BORDAS):
-            self.bordas.append(pygame.Rect((0, i*TAMANHO_BORDAS+TAMANHO_MENU), (TAMANHO_BORDAS, TAMANHO_BORDAS)))
-            self.bordas.append(pygame.Rect(((CELULAS_LARGURA*TAMANHO_CELULA)-TAMANHO_BORDAS, i*TAMANHO_BORDAS+TAMANHO_MENU), (TAMANHO_BORDAS, TAMANHO_BORDAS)))
+            x = 0 ; y = i*TAMANHO_BORDAS+TAMANHO_MENU
+            self.bordas.append((x, y))
+            x = (CELULAS_LARGURA*TAMANHO_CELULA)-TAMANHO_BORDAS ; y = i*TAMANHO_BORDAS+TAMANHO_MENU
+            self.bordas.append((x, y))
         
         for a in range (0, (TAMANHO_CELULA*CELULAS_LARGURA)//TAMANHO_BORDAS):
-            self.bordas.append(pygame.Rect((a*TAMANHO_BORDAS, TAMANHO_MENU), (TAMANHO_BORDAS, TAMANHO_BORDAS)))
-            self.bordas.append(pygame.Rect(((a*TAMANHO_BORDAS, (CELULAS_ALTURA*TAMANHO_CELULA)-TAMANHO_BORDAS),(TAMANHO_BORDAS, TAMANHO_BORDAS))))
-        
-    def desenhar_bordas(self):
-        cor_borda = (80, 80, 80)
-        for i in self.bordas:
-            pygame.draw.rect(self.tela,cor_borda, i)
+            x = a*TAMANHO_BORDAS ; y = TAMANHO_MENU
+            self.bordas.append((x, y))
+            x = a*TAMANHO_BORDAS ; y = (CELULAS_ALTURA*TAMANHO_CELULA)-TAMANHO_BORDAS
+            self.bordas.append((x, y))
+
+
+class Bordas(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("sprites\Borda.jpg")
+        self.image = pygame.transform.scale(self.image, (TAMANHO_BORDAS, TAMANHO_BORDAS))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = x, y
+            
         
