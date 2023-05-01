da5fde35a5090b36074d0a046c874889f0906e31
Flávio Roberto <124847034+MusicalUsagi@users.noreply.github.com>
2023-05-01 16:18:01 -0300

    Adicionado colisão com blocos

--- personagem.py
+++ personagem.py
@@ -6,15 +6,16 @@ class Personagem(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.tela = tela
         self.vida = 3
-        self.altura = 40
-        self.largura = 40
-        self.velocidade = 20
+        self.altura = TAMANHO_CELULA-10
+        self.largura = TAMANHO_CELULA-10
+        self.velocidade = 5
         self.image = pygame.image.load("sprites\Personagem.png")
         self.image = pygame.transform.scale(self.image, (self.largura, self.altura))
         self.rect = self.image.get_rect()
         self.rect.topleft = x, y
 
     def update(self):
+        self.x1 = self.rect.x ; self.y1 = self.rect.y
         botao = pygame.key.get_pressed()
         if botao[pygame.K_a]:
             self.rect.x -= self.velocidade
@@ -44,3 +45,7 @@ class Personagem(pygame.sprite.Sprite):
         elif self.rect.y < TAMANHO_MENU+TAMANHO_BORDAS:
            self.rect.y = TAMANHO_MENU+TAMANHO_BORDAS
         
+        for i in range (len(blocos_indestrutiveis)):
+            if self.rect.colliderect(pygame.rect.Rect(blocos_indestrutiveis[i], (TAMANHO_CELULA, TAMANHO_CELULA))):
+                self.rect.x = self.x1
+                self.rect.y = self.y1
