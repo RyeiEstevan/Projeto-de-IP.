7fe8ce0f2b828f88a55952a016855fadfaefe4bd
Gust4voSSM <gugu9santiago@gmail.com>
2023-05-01 17:42:37 -0300

    grupo sprites movido para fora da main e adicionado a um novo pacote; itens agora são instanciados na main

--- main.py
+++ main.py
@@ -4,32 +4,45 @@ from mapa import Mapa
 from mapa import Bordas
 from blocos import Blocos_indestrutiveis
 from personagem import Personagem
-
+from item import Item
+from sprites import sprites
 
 def main():
 
     #Criando objetos e o grupo de seus sprites
-    sprites = pygame.sprite.Group()
+    #sprites = pygame.sprite.Group()
     mapa = Mapa(CELULAS_ALTURA*TAMANHO_CELULA, CELULAS_LARGURA*TAMANHO_CELULA, TAMANHO_MENU, TAMANHO_BORDAS) 
     bordas = [Bordas(i[0], i[1]) for i in mapa.bordas]
     blocos_ind = [Blocos_indestrutiveis(i[0], i[1]) for i in blocos_indestrutiveis]
+
+    Item(mapa.tela, 0, 2, "velocidade")
+    Item(mapa.tela, 0, 5, "tempo")
+    Item(mapa.tela, 3, 8, "vida")
+    Item(mapa.tela, 4, 2,"bomba")
+
     player = Personagem(mapa.tela, 25, 125)
-    [sprites.add(i) for i in bordas+blocos_ind+[player]]
+    [sprites.add(i) for i in bordas + blocos_ind]
 
     #Iniciando pygame e loop para o jogo ficar rodando até fecharem
     clock = pygame.time.Clock()
     Rodar = True
     pygame.init()
     while Rodar:
+
         mapa.tela.blit(mapa.background, (0, TAMANHO_BORDAS+TAMANHO_MENU))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 Rodar == False
                 exit()
-
-
+        
+        player.sprite.draw(mapa.tela)
         sprites.draw(mapa.tela)
         sprites.update()
+        player.sprite.update()
         pygame.display.update()
         clock.tick(60)
+
+        if player.vida <= 0:
+            Rodar = False
+
 main()
