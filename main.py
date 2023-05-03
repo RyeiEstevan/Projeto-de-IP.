88f6fa38dd214856b91710d6ea5232e3a653c0c1
Ryei Estevan <riey20112003@gmail.com>
2023-05-02 21:55:39 -0300

    Criamos o sistema de contagem da vida

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def main():
 
     #Criando objetos e o grupo de seus sprites
     #sprites = pygame.sprite.Group()
+    pygame.font.init()
     mapa = Mapa(CELULAS_ALTURA*TAMANHO_CELULA, CELULAS_LARGURA*TAMANHO_CELULA, TAMANHO_MENU, TAMANHO_BORDAS) 
     bordas = [Bordas(i[0], i[1]) for i in mapa.bordas]
     blocos_ind = [Blocos_indestrutiveis(i[0], i[1]) for i in blocos_indestrutiveis]
@@ -23,18 +24,22 @@ def main():
     player = Personagem(mapa.tela, 25, 125)
     [sprites.add(i) for i in bordas + blocos_ind]
 
+    fonte = pygame.font.SysFont('arial', 40, True, True)
+
     #Iniciando pygame e loop para o jogo ficar rodando até fecharem
     clock = pygame.time.Clock()
     Rodar = True
     pygame.init()
     while Rodar:
-
+        mensagem = f'Vida: {player.vida}'
+        mapa.tela.fill((0,0,0))
+        texto_formatado = fonte.render(mensagem, False, (255, 255, 255))
         mapa.tela.blit(mapa.background, (0, TAMANHO_BORDAS+TAMANHO_MENU))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 Rodar == False
                 exit()
-        
+        mapa.tela.blit(texto_formatado, (0,0))
         player.sprite.draw(mapa.tela)
         sprites.draw(mapa.tela)
         sprites.update()
