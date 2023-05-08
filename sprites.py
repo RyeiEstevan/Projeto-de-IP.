e7640be7986c32f301ad7070746040628032244f
Gust4voSSM <gugu9santiago@gmail.com>
2023-05-08 19:04:50 -0300

    comentários no código

--- sprites.py
+++ sprites.py
@@ -4,21 +4,27 @@ sprites = Group()
 vilao = Group()
 
 sprite = {
+    # Interface
+    "inicio": "sprites\Tela_inicial1.jpg",
+    "ganhou": "sprites\Ganhou.jpg",
+    "perdeu": "sprites\Perdeu.jpg",
+    # Mapa
     "back" : "sprites\Back.png",
+    "bordas": "sprites\Bordas.png",
+    # Blocos
     "bloco_destruido": "Bloco_destruido.png",
     "bloco_destrutivel": "sprites\Bloco_destrutivel.png",
     "bloco_indestrutivel": "sprites\Blocos_indestrutiveis.png",
+    # Bomba
     "bomba": "sprites\Bomba.png",
-    "bordas": "sprites\Bordas.png",
     "fogo": "sprites\Fogo.png",
+    # Personagem e Inimigos
     "personagem": "sprites\Personagem.png",
     "polemonio": "sprites\Polemonio.png",
+    # Coletáveis
     "portal": "sprites\Portal.png",
     "tempo": "sprites\Tempo.png",
     "velocidade": "sprites\Velocidade.png",
     "vida": "sprites\Vida.png",
-    "inicio": "sprites\Tela_inicial1.jpg",
-    "ganhou": "sprites\Ganhou.jpg",
-    "perdeu": "sprites\Perdeu.jpg",
     "item_bomba": "sprites\Bomba_power_up.png"
 }
