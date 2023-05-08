4df3bd67aff363ee0b0e84c6e7913e3b2fdb656b
Gustavo Felipe <110072296+guufelipe@users.noreply.github.com>
2023-05-08 00:20:46 -0300

    Update timer.py
    Comentários adicionados.

--- timer.py
+++ timer.py
@@ -1,17 +1,22 @@
 import time
 
+#Criando a classe Timer:
 class Timer:
     
+    #Definindo o Construtor da classe Timer
     def __init__(self, value = 0):
         self.start = time.time()
         self.set(value)
 
+    #Criando a função que lê e retorna o tempo  real já reduzido:    
     def get (self) -> float:
         return time.time() - self.start
     
+    #Criando a Função que define o tempo:
     def set (self, value : float):
         self.start = time.time() - value
 
+    #Criando a Função que imprime o tempo na tela.    
     def __str__(self):
         value = self.get()
         self.sec = str(int(value))
