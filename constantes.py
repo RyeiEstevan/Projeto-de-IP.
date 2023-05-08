fcac7364c43d600528f066eed1686236839dc499
Ryei Estevan <riey20112003@gmail.com>
2023-05-08 00:33:42 -0300

    comentários

--- constantes.py
+++ constantes.py
@@ -1,8 +1,10 @@
-import random
-
+#Definindo todos os parâmetros do jogo em relação a coordenadas e tamanhos
+#Tamanho de cada posição da matriz
 TAMANHO_CELULA = 50
+#Quantidade de posições na horizontal e vertical
 CELULAS_ALTURA = 14
 CELULAS_LARGURA = 16
+#configurações de menu e bordas
 TAMANHO_MENU = 100
 X = 0 ;  Y = 1
 TAMANHO_BORDAS = int(TAMANHO_CELULA/2)
@@ -12,6 +14,7 @@ MAPA_LARGURA = CELULAS_LARGURA - int( (TAMANHO_BORDAS*2)/TAMANHO_CELULA )
 celulas = [] #celulas será uma matriz com a coordenada de cada celula
 c = []
 y = TAMANHO_MENU + TAMANHO_BORDAS
+#gerando a matriz do mapa
 for i in range (MAPA_ALTURA):
     x = TAMANHO_BORDAS
     for a in range (MAPA_LARGURA):
@@ -19,7 +22,7 @@ for i in range (MAPA_ALTURA):
         x += TAMANHO_CELULA
     celulas.append(c) ; c = []
     y += TAMANHO_CELULA
-
+#gerando uma lista com as coordenadas dos blocos indestrutíveis
 blocos_indestrutiveis = []
 for i in range (MAPA_ALTURA):
     for a in range (MAPA_LARGURA):
